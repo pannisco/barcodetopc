7879211ac3a12023c0f1e7e53e55c761a616202d
Peppe <gpannisco@gmail.com>
2025-07-13 11:02:34 -0700

    1.0 PC

---
+++ Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblIP = New System.Windows.Forms.Label()
+        Me.lblConn = New System.Windows.Forms.Label()
+        Me.lblRicevuto = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.cmbSchede = New System.Windows.Forms.ComboBox()
+        Me.btnAvvia = New System.Windows.Forms.Button()
+        Me.chkKeyboard = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblIP
+        '
+        Me.lblIP.AutoSize = True
+        Me.lblIP.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIP.Location = New System.Drawing.Point(3, 9)
+        Me.lblIP.Name = "lblIP"
+        Me.lblIP.Size = New System.Drawing.Size(58, 37)
+        Me.lblIP.TabIndex = 0
+        Me.lblIP.Text = "IP:"
+        '
+        'lblConn
+        '
+        Me.lblConn.AutoSize = True
+        Me.lblConn.Location = New System.Drawing.Point(7, 46)
+        Me.lblConn.Name = "lblConn"
+        Me.lblConn.Size = New System.Drawing.Size(131, 13)
+        Me.lblConn.TabIndex = 1
+        Me.lblConn.Text = "In attesa di connessione..."
+        '
+        'lblRicevuto
+        '
+        Me.lblRicevuto.AutoSize = True
+        Me.lblRicevuto.Location = New System.Drawing.Point(7, 97)
+        Me.lblRicevuto.Name = "lblRicevuto"
+        Me.lblRicevuto.Size = New System.Drawing.Size(0, 13)
+        Me.lblRicevuto.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'cmbSchede
+        '
+        Me.cmbSchede.FormattingEnabled = True
+        Me.cmbSchede.Location = New System.Drawing.Point(6, 17)
+        Me.cmbSchede.Name = "cmbSchede"
+        Me.cmbSchede.Size = New System.Drawing.Size(109, 21)
+        Me.cmbSchede.TabIndex = 3
+        '
+        'btnAvvia
+        '
+        Me.btnAvvia.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAvvia.Location = New System.Drawing.Point(160, 63)
+        Me.btnAvvia.Name = "btnAvvia"
+        Me.btnAvvia.Size = New System.Drawing.Size(117, 52)
+        Me.btnAvvia.TabIndex = 4
+        Me.btnAvvia.Text = "AVVIA"
+        Me.btnAvvia.UseVisualStyleBackColor = True
+        '
+        'chkKeyboard
+        '
+        Me.chkKeyboard.AutoSize = True
+        Me.chkKeyboard.Checked = True
+        Me.chkKeyboard.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkKeyboard.Location = New System.Drawing.Point(6, 44)
+        Me.chkKeyboard.Name = "chkKeyboard"
+        Me.chkKeyboard.Size = New System.Drawing.Size(94, 17)
+        Me.chkKeyboard.TabIndex = 5
+        Me.chkKeyboard.Text = "Simula tastiera"
+        Me.chkKeyboard.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 66)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(115, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "A capo automatico"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 89)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(68, 17)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "Log CSV"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(7, 110)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(140, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Made by Pannisco Software"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.chkKeyboard)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.cmbSchede)
+        Me.GroupBox1.Location = New System.Drawing.Point(283, 9)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(121, 114)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Impostazioni"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(410, 125)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.btnAvvia)
+        Me.Controls.Add(Me.lblRicevuto)
+        Me.Controls.Add(Me.lblConn)
+        Me.Controls.Add(Me.lblIP)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Barcode To PC"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblIP As Label
+    Friend WithEvents lblConn As Label
+    Friend WithEvents lblRicevuto As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents cmbSchede As ComboBox
+    Friend WithEvents btnAvvia As Button
+    Friend WithEvents chkKeyboard As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Net
+Imports System.Net.HttpListener
+Imports System.Net.NetworkInformation
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.IO
+
+
+Public Class Form1
+
+    Private listener As HttpListener
+    Private listenerTask As Task
+    Private selectedIP As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbSchede.Items.Clear()
+        CheckBox1.Checked = My.Settings.enter
+        chkKeyboard.Checked = My.Settings.sendoutput
+        chkKeyboard.Checked = My.Settings.csvsave
+        For Each nic In NetworkInterface.GetAllNetworkInterfaces()
+            If nic.OperationalStatus = OperationalStatus.Up AndAlso
+               (nic.NetworkInterfaceType = NetworkInterfaceType.Ethernet OrElse
+                nic.NetworkInterfaceType = NetworkInterfaceType.Wireless80211) Then
+                For Each ipInfo In nic.GetIPProperties().UnicastAddresses
+                    If ipInfo.Address.AddressFamily = Net.Sockets.AddressFamily.InterNetwork Then
+                        cmbSchede.Items.Add($"{nic.Name} - {ipInfo.Address}")
+                    End If
+                Next
+            End If
+        Next
+        cmbSchede.SelectedIndex = cmbSchede.SelectedItem
+        If cmbSchede.Items.Count > 0 Then cmbSchede.SelectedIndex = 0
+        lblConn.Text = "Server non avviato"
+        lblConn.BackColor = Color.LightGray
+    End Sub
+    Private Sub btnAvvia_Click(sender As Object, e As EventArgs) Handles btnAvvia.Click
+        Try
+            If cmbSchede.SelectedItem Is Nothing Then
+                MessageBox.Show("Seleziona una scheda di rete.")
+                Return
+            End If
+            cmbSchede.Enabled = False
+            selectedIP = cmbSchede.SelectedItem.ToString().
+                         Split("-"c).Last().Trim()
+            lblIP.Text = $"IP: {selectedIP}"
+            listener = New HttpListener()
+            listener.Prefixes.Add($"http://{selectedIP}:8080/")
+            listener.Start()
+
+            listenerTask = Task.Run(AddressOf StartListener)
+
+            lblConn.Text = $"Server in ascolto sulla porta 8080"
+            lblConn.BackColor = Color.LightGreen
+            btnAvvia.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show($"Errore avvio server: {ex.Message}")
+        End Try
+    End Sub
+    Private Sub StartListener()
+        While listener IsNot Nothing AndAlso listener.IsListening
+            Try
+                Dim context = listener.GetContext()
+                Dim barcode = context.Request.QueryString("data")
+
+                If Not String.IsNullOrEmpty(barcode) Then
+                    Me.Invoke(Sub() HandleBarcode(barcode))
+                End If
+                Dim buffer = Encoding.UTF8.GetBytes("OK")
+                context.Response.ContentLength64 = buffer.Length
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length)
+                context.Response.Close()
+            Catch ex As HttpListenerException
+                Exit While
+            Catch
+            End Try
+        End While
+    End Sub
+    Private Sub HandleBarcode(code As String)
+        lblRicevuto.Text = $"Ultimo codice: {code}"
+        lblRicevuto.BackColor = Color.Yellow
+        Timer1.Start()
+        If CheckBox2.Checked Then
+            Try
+                File.AppendAllText("log.csv",
+                                   $"{DateTime.Now:yyyy-MM-dd HH:mm:ss};{code}{Environment.NewLine}")
+            Catch ex As Exception
+
+            End Try
+        End If
+
+        If chkKeyboard.Checked Then
+            Try
+                SendKeys.SendWait(code & "{ENTER}")
+            Catch ex As Exception
+                MessageBox.Show($"Errore simulazione tastiera: {ex.Message}")
+            End Try
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblRicevuto.BackColor = SystemColors.Control
+        Timer1.Stop()
+    End Sub
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
+        Try
+            listener?.Stop()
+            listener?.Close()
+        Catch
+        End Try
+        MyBase.OnFormClosing(e)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        My.Settings.enter = CheckBox1.Checked
+    End Sub
+
+    Private Sub chkKeyboard_CheckedChanged(sender As Object, e As EventArgs) Handles chkKeyboard.CheckedChanged
+        My.Settings.sendoutput = chkKeyboard.Checked
+    End Sub
+
+    Private Sub cmbSchede_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbSchede.SelectedIndexChanged
+        My.Settings.schedaeth = cmbSchede.SelectedItem
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        My.Settings.csvsave = chkKeyboard.Checked
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://github.com/pannisco/barcodetopc")
+    End Sub
+End Class
